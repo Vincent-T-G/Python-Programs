43ba20e755ddb3c165577c927120245d1eca0b9a
Vincent Tirimba <vincent.tirimba05@gmail.com>
2025-04-16 03:24:55 +0300

    Made a small change to the first python project (Madlibs.py)

--- Projects/1.madlibs.py
+++ Projects/1.madlibs.py
@@ -9,6 +9,8 @@ adjective2 = input("Enter an adjective (description): ")
 verb1 = input("Enter a verb ending with 'ing: ")
 adjective3 = input("Enter an adjective (description): ")
 
+print(" ")
+
 print(f"Today I went to a {adjective1} zoo.")
 print(f"In an exhibit, I saw a {noun1}")
 print(f"{noun1} was {adjective2} and {verb1}")
